8dfa20e8dc9cb446eb9202acc4e1face46d0e2d3
Inderpreet <inderpreet2004123@gmail.com>
2024-11-22 11:12:26 -0500

    error code

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,15 +1,27 @@
+import { useState, useEffect } from 'react'
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div id="app">
       <nav>
+    <Link to="/">Home</Link>&nbsp;
+    <Link to="Form />">Form</Link>&nbsp;
+
+
         {/* NavLinks here */}
       </nav>
       {/* Route and Routes here */}
-      <Home />
+      
+      <Routes>
+        <Route path= "/" element={<Home />} />
+        <Route path= "Form" element={<Form />} />
+
+      </Routes>
+      <App />
       <Form />
     </div>
   )
